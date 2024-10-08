213095b4590176fd21e6c38360776ca1c35b4a9b
yxiao54 <62627876+yxiao54@users.noreply.github.com>
2024-10-08 01:37:01 -0700

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,23 +1,12 @@
-from models.baseline import Baseline,multitask
-from models.multimodal import  MM
 
 from models.propose import P2
-from models.lossfunction import flow2KL,flow2cmd,flow2mmd
-from models.features import feature
+
 
 Models = {
    
-    'baseline':Baseline,
-    'mm':MM,
+ 
     'p2':P2,
-    'kl':flow2KL,
-    'cmd':flow2cmd,
-    'mmd':flow2mmd,
-    'multitask':multitask,
-    'feature':feature,
+
     
 
 }
-#baseline
-#multimodal
-#halluci
